248e67665ac605ad56f3aff92fc0caf6c29470f0
M1F <it-technologies@yandex.ru>
2018-11-29 23:14:41 +0700

    Процедурный подход, функция Draw

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,6 +10,15 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Draw(1, 3, '*');
+
+            Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
